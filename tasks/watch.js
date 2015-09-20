288310d939fed2e677ed4f5e8816299d359b0469
Culttm <culttm@gmail.com>
2015-09-20 17:20:04 +0300

    added support detecting new files

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,11 +1,31 @@
 var gulp = require("gulp");
 var browserSync = require('browser-sync');
+var path   = require('path')
+var watch  = require('gulp-watch')
+
 
 gulp.task('watch', function () {
-  gulp.watch('./src/images/**/*', ['moveImages']);
-  gulp.watch('./src/assets/**/*', ['moveAssets']);
-  gulp.watch('./src/fonts/**/*', ['moveFonts']);
-  gulp.watch('./src/sass/**/*.scss', ['compass']);
-  gulp.watch('./src/coffee/**/*.coffee', ['coffee']);
-  gulp.watch('./src/**/*.html', ['moveHtml']);
+	watch('./src/images/**/*', function(){
+		gulp.start('moveImages')
+	});
+
+	watch('./src/assets/**/*', function(){
+		gulp.start('moveAssets')
+	});
+
+	watch('./src/fonts/**/*', function(){
+		gulp.start('moveFonts')
+	});
+
+	watch('./src/coffee/**/*.coffee', function(){
+		gulp.start('coffee')
+	});
+
+	watch('./src/**/*.html', function(){
+		gulp.start('moveHtml')
+	});
+
+	watch('./src/sass/**/*.scss', function(){
+		gulp.start('compass')
+	});
 });
